8d5ab8e199c974e0d0b21a91056fe40b5cba464f
MihalacheGeorge <32979930+MihalacheGeorge@users.noreply.github.com>
2017-11-08 20:37:25 +0200

    Update program_1 Mihalache George.m

--- program_1 Mihalache George.m
+++ program_1 Mihalache George.m
@@ -2,8 +2,7 @@ function tema_de_casa_1()
 
 fs = 5000;
 t = 0:1/fs:100;
-x = square(pi*t,25); %semnal dreptunghiular
-                     %perioada 2s si factorul de umplere 25%
+x = square(pi*t,25); %semnal dreptunghiular cu perioada 2s si factorul de umplere 25%
 
 for i = 1:1:length(x)
    if x(i) > 0
